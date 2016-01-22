28d49195171c8e5130f4ae3369cd70aa441050f6
michaelscotthurley <michaelscotthurley>
2016-01-21 22:25:16 -0800

    Homework 2 - JS Functions

--- google_shopping_functions.js
+++ google_shopping_functions.js
@@ -1,3 +1,6 @@
+//load products.json
+var data = require("./products.json")
+
 //define getItemsCount function
 //accepts full item data
 //returns the length of the items array
@@ -5,26 +8,62 @@ var getItemsCount = function(itemData){
 	return itemData.items.length;
 };
 
-//
-//DEFINE FUNCTIONS HERE
-//
+var getItems = function(objectData){
+	return objectData.items;
+}
 
+var items = getItems(data);
 
+var getItemsByBrand = function(items, brand) {
+	var brandItems = [];
+	for (i = 0; i < items.length; i++) {
+		if (items[i].product.brand === brand) {
+			brandItems.push(items[i]);
+		}
+	}
+	return brandItems;
+}
 
+var getItemsByAuthor = function(items, author) {
+	var authorItems = [];
+	for (i = 0; i < items.length; i++) {
+		if (items[i].product.author.name.toLowerCase().indexOf(author) >=0) {
+			authorItems.push(items[i]);
+		}
+	}
+	return authorItems;
+}
 
+var getAvailableProducts = function(items) {
+	var availableProducts = [];
+	for (i = 0; i <items.length; i++) {
+		if (items[i].product.inventories[0].availability === "inStock") {
+			availableProducts.push(items[i]);
+		}
+	}
+	return availableProducts;
+}
 
-//load products.json
-var data = require("./products.json")
+//Original function to get appended eBay items. Decided to modify the orig getItemsByAuthor instead.
+// var getEbayItems = function(items, author) {
+// 	var eBayItems = [];
+// 	for (i = 0; i < items.length; i++) {
+// 		if (items[i].product.author.name.indexOf(author) >= 0) {
+// 			eBayItems.push(items[i]);
+// 		}
+// 	}
+// 	return eBayItems;
+// }
 
-//
-//USE FUNCTIONS HERE
-//
+var sonyProducts = getItemsByBrand(items, "Sony");
+var adoramaProducts = getItemsByAuthor(items, "Adorama Camera");
+var nikonProducts = getItemsByBrand(items, "Nikon");
 
-
-
-
-
+console.log(getItemsByBrand(items, "Sony"));
+console.log(getAvailableProducts(sonyProducts));
+console.log(getAvailableProducts(adoramaProducts));
+console.log(getItemsByAuthor(nikonProducts, "ebay"));
 
 //output item count using the getItemsCount function
-console.log('Item Count: ' + getItemsCount(data));
+// console.log('Item Count: ' + getItemsCount(data));
 
