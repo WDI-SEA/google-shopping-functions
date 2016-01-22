117786613150bb45ee48ce5faf146aaddd555dfd
Ella Erickson <ellazerickson@gmail.com>
2016-01-22 07:36:51 -0800

    final of google shopping functions

--- testfile.js
+++ testfile.js
@@ -10,32 +10,30 @@ var getItemsCount = function(itemData){
 //DEFINE FUNCTIONS HERE
 //
 var getItems = function(objectData) {
-	return objectData.items;
+	return objectData.items
 }
 
 var getItemsByBrand = function(items, brand) {
 	var objectBrands = [];
 	for (var i=0; i < items.length; i++) {
-		if(items[i].product.brand.toLowerCase() == brand.toLowerCase()) {
-			objectBrands.push(items[i]);
-		}
-	} return objectBrands;
+		if(items[i].product.brand == brand)
+			objectBrands.push(items[i])
+	} return objectBrands
 }
 var getItemsByAuthor = function(items, author) {
 	var objectAuthors = [];
 	for (var i=0; i < items.length; i++) {
 		if(items[i].product.author.name.toLowerCase().indexOf(author.toLowerCase()) != -1) {
-			objectAuthors.push(items[i]);
+			objectAuthors.push(items[i])
 		}
-	} return objectAuthors;
+	} return objectAuthors
 }
 var getAvailableItems = function(items) {
 	var objectAvail = [];
 	for (var i=0; i < items.length; i++) {
-		if(items[i].product.inventories[0].availability == "inStock") {
-			objectAvail.push(items[i]);
-		}
-	} return objectAvail;
+		if(items[i].product.inventories[0].availability == "inStock")
+			objectAvail.push(items[i])
+	} return objectAvail
 }
 
 
@@ -50,41 +48,27 @@ var items = getItems(data)
 // var items = getItems(items);
 
 // All items made by Sony
-var itemsBySony = getItemsByBrand(items, 'Sony');
-console.log(itemsBySony);
+var itemsByBrand = getItemsByBrand(items, 'Sony');
+// console.log(itemsByBrand);
 
-console.log("---------------break--------------");
+console.log("---------------break--------------")
 
 //not sure how to get below to work for combined functions
 // var availSony = getItemsByBrand(items, 'Sony') && getAvailableItems(items);
 // console.log(availSony);
 
-var availItems = getAvailableItems(items);
-// console.log(availItems);
 
 
-var availItemsbySony = getAvailableItems(itemsBySony);
-console.log(availItemsbySony);
+var availItemsbyBrand = getAvailableItems(itemsByBrand);
+// console.log(availItemsbyBrand);
 
-console.log("---------------break--------------");
+var itemsByAuthor = getItemsByAuthor(items, "Adorama Camera");
+console.log(itemsByAuthor);
 
-var itemsByAdorama = getItemsByAuthor(items, "Adorama Camera");
-console.log(itemsByAdorama);
+console.log("---------------break--------------")
 
-// var availItemsbyAdorama = getAvailableItems(itemsByAdorama);
-// console.log(availItemsbyAdorama);
-
-console.log("---------------break--------------");
-
-var ebayItems = getItemsByAuthor(items, "ebay");
-// console.log(ebayItems);
-
-var ebayNikonItems = getItemsByBrand(ebayItems, 'Nikon');
-console.log(ebayNikonItems);
-
-
-
-console.log("---------------break--------------");
+var availItemsbyAuthor = getAvailableItems(itemsByAuthor);
+console.log(availItemsbyAuthor);
 
 
 
